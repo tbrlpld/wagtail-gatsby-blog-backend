9a749b4d29d27f56f78a6b543c764f8e81dfcc96
Tibor Leupold <tibor@lpld.io>
2020-06-16 11:10:02 -1000

    Create custom schema to allow extension of grapple schema

---
+++ mysite/schema.py
@@ -0,0 +1,26 @@
+import graphene
+
+from grapple import schema as gs
+
+
+GrappleQuery = gs.schema._query
+GrappleSubscription = gs.schema._subscription
+GrappleMutation = gs.schema._mutation
+
+
+class Query(GrappleQuery):
+    pass
+
+
+# class Mutation(GrappleMutation):
+#     pass
+
+
+class Subscription(GrappleSubscription):
+    pass
+
+
+schema = graphene.Schema(
+    query=Query,
+    subscription=Subscription,
+)
