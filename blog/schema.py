1ffa4e62c86f5a0e09a0119dfd0134ed7c79b94d
Tibor Leupold <tibor@lpld.io>
2020-06-17 13:48:45 -1000

    Return image objects with tag

--- blog/schema.py
+++ blog/schema.py
@@ -6,40 +6,36 @@ Custom GraphQL schema types that are not supported natively by Grapple.
 """
 
 import graphene
+import graphene_django
 from graphene_django.converter import convert_django_field
-import grapple.models as gpl
+import grapple.models as gpm
 import modelcluster.contrib.taggit as mct
 import taggit.models as tgt
 
 
-@convert_django_field.register(mct.ClusterTaggableManager)
-def convert_tag_manager_to_string(field, registry=None):
-    """Define converter type for ClusterTaggableManager."""
-    return TagType()
 
+class TagType(graphene_django.DjangoObjectType):
+    class Meta:
+        model = tgt.Tag
 
-class TagType(graphene.ObjectType):
-    """Tag GraphQL Type."""
+    tagged_items = graphene.List("grapple.types.images.ImageObjectType")
+    # tagged_items = graphene.Field(TaggedItemType)
 
-    tag_id = graphene.Int(name='id')
-    name = graphene.String()
-    slug = graphene.String()
-
-    def resolve_tag_id(self, _):  # noqa: D102
-        return self.id
-
-    def resolve_name(self, _):  # noqa: D102
-        return self.name
-
-    def resolve_slug(self, _):  # noqa: D102
-        return self.slug
-
+    def resolve_tagged_items(self, _):
+        tagged_items = self.taggit_taggeditem_items.all()
+        native_items = []
+        for tagged_item in tagged_items:
+            item_class = tagged_item.content_type.model_class()
+            native_item = item_class.objects.get(pk=tagged_item.pk)
+            native_items.append(native_item)
+        return native_items
+        # return self.taggit_taggeditem_items.first().content_type.model_class().objects.all()
 
 
 class TagQuery(graphene.ObjectType):
     """Queries for the TagType."""
 
-    tag = graphene.Field(TagType, id=graphene.Int())
+    tag = graphene.Field(TagType, id=graphene.ID())
     tags = graphene.List(TagType)
 
     def resolve_tag(self, _, id):  # noqa: D102
@@ -49,9 +45,15 @@ class TagQuery(graphene.ObjectType):
         return tgt.Tag.objects.all().order_by('pk')
 
 
+@convert_django_field.register(mct.ClusterTaggableManager)
+def convert_tag_manager_to_string(field, registry=None):
+    """Define converter type for ClusterTaggableManager."""
+    return TagType()
+
+
 def GraphQLTags(field_name: str, **kwargs):  # noqa: N802
     """Custom grapple wrapper function for the TagType."""
     def Mixin():
-        return gpl.GraphQLField(field_name, TagType, is_list=True, **kwargs)
+        return gpm.GraphQLField(field_name, TagType, is_list=True, **kwargs)
 
     return Mixin
