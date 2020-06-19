c386025265e0648bcef4063ca58e2a9891c93a5b
Tibor Leupold <tibor@lpld.io>
2020-06-18 15:45:13 -1000

    Minor renaming and refactoring in tagging.schema

--- tagging/schema.py
+++ tagging/schema.py
@@ -3,29 +3,33 @@
 """
 Custom GraphQL schema types that are not supported natively by Grapple.
 
-"""
+Importing the grapple types does not work, because they depend on the
+apps being registered.
 
-import django.db.models as djm
-import graphene
-import graphene_django
-import taggit.models as tgt
-
-from wagtail.documents.models import get_document_model
-from wagtail.images import get_image_model
-
-
-# Importing the grapple types does not work, because they depend on the
-# apps being registered.
-# from grapple.types.images import ImageObjectType  # noqa: E800
+```python
+from grapple.types.images import ImageObjectType  # noqa: E800
 # -> django.core.exceptions.AppRegistryNotReady: Models aren't loaded yet.
+```
 
-# graphene.Union fails. It can not work with the string identified types.
-# Therefore, I can not return all tagged items in one type. The different
-# returned elements need to have their own tag type each.
+graphene.Union fails. It can not work with the string identified types
+the way that graphene.List does.
+Therefore, I can not return all tagged items in one type. The different
+returned elements need to have their own tag type each.
+
+"""
+from django.db import models as djm  # type: ignore
+
+from wagtail.documents.models import get_document_model   # type: ignore
+from wagtail.images import get_image_model   # type: ignore
+
+from taggit import models as tgm   # type: ignore
+
+import graphene  # type: ignore
+import graphene_django  # type: ignore
 
 
 def convert_tagged_items_to_model_queryset(
-    tag: tgt.Tag,
+    tag: tgm.Tag,
     target_model: djm.Model,
 ):
     """
@@ -48,7 +52,6 @@ def convert_tagged_items_to_model_queryset(
         with the given `tag`.
 
     """
-
     tagged_items = tag.taggit_taggeditem_items.all()
     tagged_item_pks = []
     for ti in tagged_items:
@@ -64,12 +67,12 @@ def convert_tagged_items_to_model_queryset(
 
 class TagType(graphene_django.DjangoObjectType):
     class Meta:
-        model = tgt.Tag
+        model = tgm.Tag
 
 
 class ImageTagType(graphene_django.DjangoObjectType):
     class Meta:
-        model = tgt.Tag
+        model = tgm.Tag
 
     tagged_images = graphene.List("grapple.types.images.ImageObjectType")
 
@@ -81,7 +84,7 @@ class ImageTagType(graphene_django.DjangoObjectType):
 
 class DocumentTagType(graphene_django.DjangoObjectType):
     class Meta:
-        model = tgt.Tag
+        model = tgm.Tag
 
     tagged_documents = graphene.List("grapple.types.documents.DocumentObjectType")
 
@@ -103,32 +106,32 @@ class Query(graphene.ObjectType):
     document_tag = graphene.Field(DocumentTagType, id=graphene.ID())
     document_tags = graphene.List(DocumentTagType)
 
-    def resolve_tag(self, _, id):  # noqa: D102
-        return tgt.Tag.objects.get(pk=id)
+    def resolve_tag(self, _, id):
+        return tgm.Tag.objects.get(pk=id)
 
     def resolve_tags(self, _):  # noqa: D102
-        return tgt.Tag.objects.all().order_by('pk')
+        return tgm.Tag.objects.all().order_by('pk')
 
     def resolve_image_tag(self, _, id):
-        return tgt.Tag.objects.filter(
+        return tgm.Tag.objects.filter(
             taggit_taggeditem_items__content_type__model='image',
         ).get(
             pk=id,
         )
 
     def resolve_image_tags(self, _):
-        return tgt.Tag.objects.filter(
+        return tgm.Tag.objects.filter(
             taggit_taggeditem_items__content_type__model='image',
         )
 
     def resolve_document_tag(self, _, id):
-        return tgt.Tag.objects.filter(
+        return tgm.Tag.objects.filter(
             taggit_taggeditem_items__content_type__model='document',
         ).get(
             pk=id,
         )
 
     def resolve_document_tags(self, _):
-        return tgt.Tag.objects.filter(
+        return tgm.Tag.objects.filter(
             taggit_taggeditem_items__content_type__model='document',
         )
