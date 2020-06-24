4004d2f8fba789c72e698fbbef8e24cb9bd36d9c
Tibor Leupold <tibor@lpld.io>
2020-06-24 12:01:43 -1000

    Revert "Make blogpages available from category"
    This reverts commit baa03af4d5ec7287718532ba9e7b0d95a4b77b40.

--- blog/models.py
+++ blog/models.py
@@ -37,12 +37,8 @@ class BlogCategory(djm.Model):
 
     graphql_fields = [
         gpm.GraphQLString('name'),
-        gpm.GraphQLCollection(
-            gpm.GraphQLForeignKey,
-            'blogpages',
-            'blog.BlogPage',
-        )
     ]
+
 
     def __str__(self):
         return self.name
