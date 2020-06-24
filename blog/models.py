58a2830e8bdb4865352ca1e7da22ba8da520b6d3
Tibor Leupold <tibor@lpld.io>
2020-06-23 16:32:15 -1000

    Allow query for blog page's category

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,11 @@ from wagtail.images.blocks import ImageChooserBlock
 from wagtail.search import index
 from wagtail.snippets import models as wtsnip
 
+from grapple import helpers as gph
 from grapple import models as gpm
 
 
+@gph.register_query_field('category', 'categories')
 @wtsnip.register_snippet
 class BlogCategory(djm.Model):
     name = djm.CharField(max_length=255)
@@ -32,6 +34,11 @@ class BlogCategory(djm.Model):
         FieldPanel('name'),
         ImageChooserPanel('icon'),
     ]
+
+    graphql_fields = [
+        gpm.GraphQLString('name'),
+    ]
+
 
     def __str__(self):
         return self.name
@@ -89,6 +96,7 @@ class BlogPageTag(TaggedItemBase):
     )
 
 
+@gph.register_query_field('blogpage')
 class BlogPage(Page):
     parent_page_types = [
         BlogIndexPage,
@@ -146,6 +154,7 @@ class BlogPage(Page):
         gpm.GraphQLString("author"),
         gpm.GraphQLString("intro"),
         gpm.GraphQLString("body"),
+        gpm.GraphQLStreamfield("freeformbody"),
         gpm.GraphQLField(
             'tags',
             'tagging.schema.TagType',
@@ -158,7 +167,7 @@ class BlogPage(Page):
             'blog.schema.BlogPageTagConnection',
             is_list=True,
         ),
-        gpm.GraphQLStreamfield("freeformbody"),
+        gpm.GraphQLSnippet('category', 'blog.BlogCategory'),
     ]
 
 
