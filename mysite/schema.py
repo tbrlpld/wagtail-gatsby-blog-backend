2c17556c69088ebc572a8838c98d5e233919aa29
Tibor Leupold <tibor@lpld.io>
2020-06-25 11:27:29 -1000

    Make all types available in schema
    Before, after using my merged schema, I was not able to get the content of
    stream fields properly any more.
    
    This was because types where missing that define the GraphQL representation
    of the various fields that are possible in a StreamField.
    
    This is fixed now.
    
    "

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,27 +1,29 @@
 import graphene
-from grapple. schema import schema as gs
+from grapple.schema import schema as gs
 
 from tagging.schema import Query as TaggingQuery
-# from blog.schema import Query as BlogQuery
 
-GrappleQuery = gs._query
-GrappleSubscription = gs._subscription
-GrappleMutation = gs._mutation
+GrappleQuery = gs.Query
+GrappleSubscription = gs.Subscription
 
 
 class Query(
     GrappleQuery,
     TaggingQuery,
-    # BlogQuery,
 ):
     pass
 
 
-class Subscription(GrappleSubscription):
+class Subscription(
+    GrappleSubscription,
+):
     pass
 
 
+types = gs.types + []
+
 schema = graphene.Schema(
     query=Query,
-    subscription=GrappleSubscription,
+    subscription=Subscription,
+    types=types,
 )
