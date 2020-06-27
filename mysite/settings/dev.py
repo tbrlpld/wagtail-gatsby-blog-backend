4dd01ee18ed06e2d1e4252b0d98df34610ad1903
Tibor Leupold <tibor@lpld.io>
2020-06-26 15:31:29 -1000

    Set base url for dev to localhost
    This is so that media URLs are actually being served under the generated URL.

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -7,10 +7,11 @@ DEBUG = True
 SECRET_KEY = 'a*9eh5fcp0f3)5b@2(61-j(*8afyzgt*mx@-9s9403n&0t3!)7'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+BASE_URL = 'http://localhost:8000'
 
 try:
     from .local import *
