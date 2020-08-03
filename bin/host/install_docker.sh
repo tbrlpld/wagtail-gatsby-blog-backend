bee5c1af7bc4d7c97c9ef336e549d2909f593ed4
Tibor Leupold <tibor@lpld.io>
2020-08-03 11:28:38 -1000

    Add install instructions for docker-compose

--- bin/host/install_docker.sh
+++ bin/host/install_docker.sh
@@ -1,15 +1,33 @@
-# Running as root () or sudoer
+# Running as root (or sudoer)
 apt-get update
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.i
+if ! command -v docker &> /dev/null
+then 
+  echo "Docker not found. Installing..."
+  apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+  add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+  apt-get update
+  apt-get install -y docker-ce docker-ce-cli containerd.i
+else
+  echo "Docker is already installed"
+fi
+docker --version
+
+# Install docker-compose
+if ! command -v docker-compose &> /dev/null 
+then 
+  echo "docker-compose not found. Installing..."
+  curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+else 
+  echo "docker-compose is already installed"
+fi
+docker-compose --version
