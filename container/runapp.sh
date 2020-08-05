1e6f451b14af44ca42c1b45c5e219bff4777b730
Tibor Leupold <tibor@lpld.io>
2020-08-04 16:30:09 -1000

    Make output of runapp.sh more clear

--- container/runapp.sh
+++ container/runapp.sh
@@ -6,12 +6,13 @@ echo $$ > /code/APPPID
 export VENV=$(poetry env info --path)
 if [ $DJANGO_SETTINGS_MODULE = "mysite.settings.dev" ]  
 then 
-	echo "Development"
+	echo "Detected development environment."
 	# In development, the local repository should be mounted as a volume and changes are available that way.
 	$VENV/bin/gunicorn mysite.wsgi:application --bind 0.0.0.0:8000 --workers 3 --reload  --error-logfile - --log-file - --log-level debug
 else 
-	echo "Production"
+	echo "Detected production environment."
 	# In production the repo does not need to be present on the host. The repo changes are pulled right into the container before the app starts.
+	echo "Pulling new code fro repo..."
 	git pull --ff-only
 	$VENV/bin/python manage.py migrate
 	$VENV/bin/python manage.py collectstatic --noinput
