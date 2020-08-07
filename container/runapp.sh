b2d4a827a0a56f4ade6082cfe29ea1e7d0fcdd9b
Tibor Leupold <tibor@lpld.io>
2020-08-06 18:08:23 -1000

    Make runapp more robust for different shells

--- container/runapp.sh
+++ container/runapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export VENV=$(poetry env info --path)
-if [ $DJANGO_SETTINGS_MODULE = "mysite.settings.dev" ]  
+if [ "$DJANGO_SETTINGS_MODULE" = "mysite.settings.dev" ]  
 then 
 	echo "Detected development environment."
 	# In development, the local repository should be mounted as a volume and changes are available that way.
