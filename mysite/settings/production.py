35d3f3912e653756e716c1ad7a902baa5cd30395
Tibor Leupold <tibor@lpld.io>
2020-08-11 16:16:02 -1000

    Update production settings

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -6,9 +6,16 @@ DEBUG = False
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+BASE_URL = 'http://test.lpld.io'
+
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    'test.lpld.io',
+]
+CORS_ORIGIN_WHITELIST = [
+    BASE_URL,
+    "http://localhost:9000",
 ]
 
 try:
