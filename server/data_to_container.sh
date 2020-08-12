46387168e4e311233e7578df1d14b2bced1a7457
Tibor Leupold <tibor@lpld.io>
2020-08-12 11:34:45 -1000

    Quitely pull images in "data to container" script

--- server/data_to_container.sh
+++ server/data_to_container.sh
@@ -1,6 +1,6 @@
 scriptdir=$(realpath $(dirname $0))
 basedir=$(realpath $(dirname $scriptdir))
 
-sudo -H -u dockrunner docker-compose -f $basedir/docker-compose.yml pull
+sudo -H -u dockrunner docker-compose -f $basedir/docker-compose.yml pull -q
 sudo -H -u dockrunner docker-compose -f $basedir/docker-compose.yml up --no-start
 sudo -H -u dockrunner docker cp $basedir/data wagtail-gatsby-blog-wagtail:/code
