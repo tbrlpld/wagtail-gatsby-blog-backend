e4406b89712e005c41982581a608b21f3cbc539c
Tibor Leupold <tibor@lpld.io>
2020-08-12 13:30:10 -1000

    Remove printing of url patterns in debug mode

--- mysite/urls.py
+++ mysite/urls.py
@@ -29,7 +29,6 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    print(urlpatterns)
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
